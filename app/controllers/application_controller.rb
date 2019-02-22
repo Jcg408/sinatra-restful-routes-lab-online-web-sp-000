ac9c301708bf6a8a84a163e38c27106a388b90dd
Jessica Gladstone <jcg321@comcast.net>
2019-02-22 01:08:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
   
   
   post '/recipes' do
-    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cooking_time => params[:cooking_time])
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     redirect '/recipes/#{@recipe.id}'
   end
   
